5a1ab1867b231172d237a449eed369a6e05fa5f8
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2020-07-17 17:56:42 +0200

    feat: create sample

---
+++ .azure/create-az-resources.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+. ../.env
+
+local_ip=$(curl -s 'https://api.ipify.org?format=json' | jq -r .ip)
+
+az group create \
+	--name $AZ_RESOURCE_GROUP \
+	--location $AZ_LOCATION \
+	| jq
+
+az postgres server create \
+	--resource-group $AZ_RESOURCE_GROUP \
+	--name $POSTGRESQL_HOST \
+	--location $AZ_LOCATION \
+	--sku-name GP_Gen5_2 \
+	--version 11 \
+	--storage-size 20480 \
+	--admin-user $POSTGRESQL_USERNAME \
+	--admin-password $POSTGRESQL_PASSWORD \
+	--assign-identity \
+	| jq
+
+az postgres server firewall-rule create \
+	--resource-group $AZ_RESOURCE_GROUP \
+	--name $POSTGRESQL_HOST-database-allow-local-ip \
+	--server $POSTGRESQL_HOST \
+	--start-ip-address $local_ip \
+	--end-ip-address $local_ip \
+        | jq
+
+az postgres db create \
+	--resource-group $AZ_RESOURCE_GROUP \
+	--name demo \
+	--server-name $POSTGRESQL_HOST \
+	| jq
+
---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mcr.microsoft.com/java/jre-headless:11-zulu-alpine AS base
+EXPOSE 8080
+
+FROM mcr.microsoft.com/java/jdk:11-zulu-alpine AS build
+WORKDIR /build
+
+COPY mvnw .
+COPY .mvn .mvn
+COPY pom.xml .
+RUN ./mvnw -B dependency:go-offline 
+
+COPY src src
+RUN ./mvnw -B package -DskipTests
+
+FROM base AS final
+WORKDIR /app
+ARG appInsightsAgentURL="https://github.com/microsoft/ApplicationInsights-Java/releases/download/3.0.0-PREVIEW.5/applicationinsights-agent-3.0.0-PREVIEW.5.jar"
+RUN wget -q -O applicationinsights-agent-3.0.0.jar $appInsightsAgentURL
+COPY ApplicationInsights.json .
+COPY --from=build /build/target/spring-data-jpa-demo-*.jar spring-data-jpa-demo.jar
+ENTRYPOINT ["java", "-javaagent:./applicationinsights-agent-3.0.0.jar", "-jar", "spring-data-jpa-demo.jar"]
