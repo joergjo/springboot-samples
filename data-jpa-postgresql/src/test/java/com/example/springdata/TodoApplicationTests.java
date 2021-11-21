c508e42fa615a787243b5a9c433d9016fd811fc0
Jörg Jooss <joerg@joergjooss.de>
2021-11-21 22:21:04 +0100

    feat: Spring Boot 2.5.7, CommandLineRunner, Postgres 13, update to Compose 3.9

--- data-jpa-postgresql/src/test/java/com/example/springdata/TodoApplicationTests.java
+++ data-jpa-postgresql/src/test/java/com/example/springdata/TodoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringDataJpaDemoApplicationTests {
+class TodoApplicationTests {
 
 	@Test
 	void contextLoads() {
