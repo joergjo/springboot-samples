f1c5f42e012c6d7262d04b888a934f83473f0172
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2021-11-22 11:52:42 +0100

    fix: bump acr-build.sh to 2.5.7

--- data-jpa-postgresql/src/main/java/com/example/springdata/TodoApplication.java
+++ data-jpa-postgresql/src/main/java/com/example/springdata/TodoApplication.java
@@ -11,17 +11,16 @@ import org.springframework.context.annotation.Profile;
 @SpringBootApplication
 public class TodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 
-	@Profile("dev")
+    @Profile("dev")
     @Bean
     public CommandLineRunner init(TodoRepository repository) {
         return args -> {
             var entities = Arrays.asList(
-                    new Todo("configuration", "congratulations, you have set up your Azure Container App correctly!",
-                            false),
+                    new Todo("configuration", "congratulations, you have set up JPA correctly!", false),
                     new Todo("test", "check if this is working", false),
                     new Todo("demo", "show to friends and family", false));
             repository.deleteAll();
--- data-jpa-postgresql/tools/acr-build.sh
+++ data-jpa-postgresql/tools/acr-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=${1:-2.5.6}
+version=${1:-2.5.7}
 repo=springboot-samples/data-jpa-postgresql
 az acr login
 az acr build -t ${repo}:latest -t ${repo}:${version}-{{.Run.ID}} -f ./Dockerfile .
