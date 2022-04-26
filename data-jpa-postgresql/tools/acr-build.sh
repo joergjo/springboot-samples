ccd4ead63ca56ae6eb5d2a83418390dc60f0726e
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2022-04-26 14:33:06 +0200

    feat: Spring Boot 2.6.7, App Insights 3.2.11

--- data-jpa-postgresql/tools/acr-build.sh
+++ data-jpa-postgresql/tools/acr-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=${1:-2.6.6}
+version=${1:-2.6.7}
 repo=springboot-samples/data-jpa-postgresql
 az acr login
 az acr build -t ${repo}:latest -t ${repo}:${version}-{{.Run.ID}} -f ./Dockerfile .
