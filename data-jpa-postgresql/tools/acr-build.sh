62d84d748dbb70bf59c002cce1ccdec2a27c93ca
Jörg Jooss <joergjo@microsoft.com>
2024-01-26 10:22:26 +0100

    refactor: remove ACT build script

--- data-jpa-postgresql/tools/acr-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-version=${1:-2.7.8}
-platform=${2:-"linux/arm64"}
-repo=springboot-samples/data-jpa-postgresql
-az acr login
-az acr build -t "${repo}:${version}-{{.Run.ID}}" --platform "${platform}"  -f ./Dockerfile .
