050c1060507004069fb3db6b777029dd19d50e2b
Jörg Jooss <joergjo@microsoft.com>
2024-02-26 13:53:53 +0100

    feat: pull based App Config refresh

--- appconfig/src/main/java/com/example/azure/appconfig/demo/HelloController.java
+++ appconfig/src/main/java/com/example/azure/appconfig/demo/HelloController.java
@@ -1,18 +1,29 @@
 package com.example.azure.appconfig.demo;
 
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.azure.spring.cloud.appconfiguration.config.AppConfigurationRefresh;
 
 @RestController
 public class HelloController {
     private final MessageProperties properties;
+
+    @Autowired(required = false)
+    private AppConfigurationRefresh refresh;
 
     public HelloController(MessageProperties properties) {
         this.properties = properties;
     }
 
     @GetMapping
-    public String getMessage() {
+    public String getMessage() throws InterruptedException, ExecutionException {
+        if (refresh != null) {
+            refresh.refreshConfigurations();
+        }
         return "Message: " + properties.getMessage();
     }
 }
